21befbd8ed7946f4e542f80b9aedf38e47b1ada2
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-04-28 18:15:13 +0200

    Zaj9

---
+++ Zaj9/Lab9/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgFirst = new System.Windows.Forms.DataGridView();
+            this.dgSecond = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSecond)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgFirst
+            // 
+            this.dgFirst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFirst.Location = new System.Drawing.Point(12, 12);
+            this.dgFirst.Name = "dgFirst";
+            this.dgFirst.Size = new System.Drawing.Size(558, 212);
+            this.dgFirst.TabIndex = 0;
+            // 
+            // dgSecond
+            // 
+            this.dgSecond.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSecond.Location = new System.Drawing.Point(12, 244);
+            this.dgSecond.Name = "dgSecond";
+            this.dgSecond.Size = new System.Drawing.Size(558, 197);
+            this.dgSecond.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 441);
+            this.Controls.Add(this.dgSecond);
+            this.Controls.Add(this.dgFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSecond)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgFirst;
+        private System.Windows.Forms.DataGridView dgSecond;
+    }
+}
+
---
+++ Zaj9/Lab9/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        private DataClasses1DataContext db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DataClasses1DataContext();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            var query1 = db.lab9s.GroupBy(v => v.DeparmentName).Select(v => new { departmentName = v.Key, maxSalary = v.Max(row => row.Rate) });
+            dgFirst.DataSource = query1;
+
+            var query2 = db
+                .lab9s
+                .GroupBy(v => new { v.DeparmentName })
+                .Select(v => new { DeparmentName = v.Key.DeparmentName, Rate = v.Max(row => row.Rate) })
+                .Join(db.lab9s, row => new { row.DeparmentName, row.Rate }, lab9 => new { lab9.DeparmentName, lab9.Rate }, (r, l) => new { departmentName = r.DeparmentName, employeeName = l.Name, rate = r.Rate })
+                .OrderByDescending(row => row.rate);
+            dgSecond.DataSource = query2;
+        }
+    }
+}
