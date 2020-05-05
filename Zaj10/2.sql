7f9bf445697ae630932364062ad9e62ec412317d
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-05-05 17:17:43 +0200

    FixZaj10

--- Zaj10/2.sql
+++ Zaj10/2.sql
@@ -7,7 +7,7 @@ AS
 	SELECT
 		FirstName,
 		LastName,
-		Demographics.value('declare default element namespace "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";(/IndividualSurvey/NumberChildrenAtHome)[1][. >1]','INT') - Demographics.value('declare default element namespace "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";(/IndividualSurvey/TotalChildren)[1][. >1]','INT') as Diff
+		Demographics.value('declare default element namespace "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";(/IndividualSurvey/NumberChildrenAtHome)[1][. >0]','INT') - Demographics.value('declare default element namespace "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";(/IndividualSurvey/TotalChildren)[1][. >0]','INT') as Diff
 	FROM Person.Person
 )
 SELECT FirstName, LastName, Diff FROM Result_CTE ORDER BY Diff DESC;
