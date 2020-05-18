e5547d6333ce477dec38fea188d42d8ff0d8061a
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-05-18 19:40:26 +0200

    Zaj11

---
+++ Zaj11/Lab11/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgFirst = new System.Windows.Forms.DataGridView();
+            this.dgSecond = new System.Windows.Forms.DataGridView();
+            this.dgThird = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgThird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgFirst
+            // 
+            this.dgFirst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFirst.Location = new System.Drawing.Point(12, 12);
+            this.dgFirst.Name = "dgFirst";
+            this.dgFirst.Size = new System.Drawing.Size(945, 150);
+            this.dgFirst.TabIndex = 0;
+            // 
+            // dgSecond
+            // 
+            this.dgSecond.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSecond.Location = new System.Drawing.Point(12, 188);
+            this.dgSecond.Name = "dgSecond";
+            this.dgSecond.Size = new System.Drawing.Size(945, 150);
+            this.dgSecond.TabIndex = 1;
+            // 
+            // dgThird
+            // 
+            this.dgThird.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgThird.Location = new System.Drawing.Point(12, 371);
+            this.dgThird.Name = "dgThird";
+            this.dgThird.Size = new System.Drawing.Size(945, 150);
+            this.dgThird.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 533);
+            this.Controls.Add(this.dgThird);
+            this.Controls.Add(this.dgSecond);
+            this.Controls.Add(this.dgFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgThird)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgFirst;
+        private System.Windows.Forms.DataGridView dgSecond;
+        private System.Windows.Forms.DataGridView dgThird;
+    }
+}
+
---
+++ Zaj11/Lab11/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Lab11
+{
+    public partial class Form1 : Form
+    {
+        public class Query1ResultClass
+        {
+            public string name;
+            public string income;
+
+            public Query1ResultClass(string n, string i)
+            {
+                name = n;
+                income = i;
+            }
+        }
+
+        private DataClasses1DataContext db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DataClasses1DataContext();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XNamespace nsp = "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/IndividualSurvey";
+            XNamespace ns = "http://schemas.microsoft.com/sqlserver/2004/07/adventure-works/Resume";
+
+            var query1 = db.Persons.Select(p => new { name = p.LastName, demographics = p.Demographics });
+
+            var val = new List<Query1ResultClass>();
+
+            foreach (var d in query1)
+            {
+                var qry = from name in ((XElement)d.demographics).Descendants(nsp + "YearlyIncome")
+                          select name.Value;
+                foreach (var q in qry)
+                {
+                    val.Add(new Query1ResultClass(d.name, q));
+                }
+            }
+
+            var val2 = new List<string>();
+
+            foreach (var d in query1)
+            {
+                var qry = from name in ((XElement)d.demographics).Descendants(nsp + "NumberChildrenAtHome") where name.Value.Equals("5")
+                          select 1;
+                foreach (var q in qry)
+                {
+                    if (q == 1) {
+                        val2.Add(d.name);
+                    }
+                }
+            }
+
+            var query3 = db.JobCandidates.Select(j => new { resume = j.Resume });
+
+            var val3 = new List<string>();
+
+            foreach (var d in query3)
+            {
+                XElement resumeXml = d.resume;
+
+                var qry2 = from add in resumeXml.Elements(ns + "Address")
+                          select add;
+
+                string result = qry2.First().Element(ns + "Addr.Location").Element(ns + "Location").Element(ns + "Loc.City").Value + " " + qry2.First().Element(ns + "Addr.Street").Value;
+                val3.Add(result);
+            }
+
+            dgFirst.DataSource = val.ConvertAll(x => new { name = x.name, income = x.income });
+            dgSecond.DataSource = val2.ConvertAll(x => new { name = x });
+            dgThird.DataSource = val3.ConvertAll(x => new { data = x });
+        }
+    }
+}
